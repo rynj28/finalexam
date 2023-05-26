7551bb2fb3183082c73c3f52e9e79b90b6886a24
SolsticeJGPS <128677494+SolsticeJGPS@users.noreply.github.com>
2023-05-26 02:07:46 -0700

    Add files via upload

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,21 @@ class Customer < ApplicationRecord
   has_many :suppliers
   has_one_attached :avatar
 
+  validate :acceptable_image
 
+   def acceptable_image
+     return unless avatar.attached?
+
+     unless avatar.blob.byte.size <= 1.megabyte
+       errors.add(:avatar, "is too big")
+     end
+
+     acceptable_types = ("image/jpeg", "image/png")
+     unless acceptable_types.include?(avatar.content_type)
+     end
+   end
+
+ >>>>>>> main
   def full_name
     "#{first_name} #{last_name}"
 end
