2540b3b2966b2c7f2da00215982034d6a356ee6d
rgbuscar <132348224+rgbuscar@users.noreply.github.com>
2023-05-26 17:56:00 +0800

    Add files via upload

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,23 +1,24 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :items
-  has_many :products, : :suppliers
+  has_many :products, through: :suppliers
   has_many :suppliers
   has_one_attached :avatar
   validate :acceptable_image
 
-  def acceptable_image
-    return unless avatar.attached?
+   def acceptable_image
+     return unless avatar.attached?
 
-    unless avatar.blob.byte.size <= 1.megabyte
-      errors.add(:avatar, "is too big")
-    end
+     unless avatar.blob.byte.size <= 1.megabyte
+       errors.add(:avatar, "is too big")
+     end
 
-    acceptable_types = ("image/jpeg", "image/png")
-    unless acceptable_types.include?(avatar.content_type)
-    end
-  end
-  
+     acceptable_types = ["image/jpeg", "image/png"]
+     unless acceptable_types.include?(avatar.content_type)
+     end
+   end
+
   def full_name
     "#{first_name} #{last_name}"
+  end
 end
